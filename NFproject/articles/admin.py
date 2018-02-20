7d2aaad3ad5f585a4c671ac1baa2d6472dcce89b
Anwarsh2018 <anwar1991@windowslive.com>
2018-02-20 15:33:48 +0300

    AdminDash Completed

--- NFproject/articles/admin.py
+++ NFproject/articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Article
+
+class articleAdmin (admin.ModelAdmin):
+  list_display= ('Title','Author','Date_created')
+
+
+admin.site.register(Article,articleAdmin)
