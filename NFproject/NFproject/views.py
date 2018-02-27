bc34f49750d4da4eb7e172826fb7bd347af963d0
Anwarsh2018 <anwar1991@windowslive.com>
2018-02-27 14:17:39 +0300

    showing the upcoming events on the home page

--- NFproject/NFproject/views.py
+++ NFproject/NFproject/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from articles.models import Article, Category
+from Events.models import Events
+from datetime import date
 
 
 def home(request):
@@ -8,4 +10,5 @@ def home(request):
     context['articles'] = Article.objects.all()
     context['featured_articles'] = Article.objects.filter(featured=True)
     context['categories'] = Category.objects.all()
+    context['upcoming_events'] = Events.objects.all().filter(date__gte=date.today()).order_by('date')
     return render(request, "home.html", context=context)
