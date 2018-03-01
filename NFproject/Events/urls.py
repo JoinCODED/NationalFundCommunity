49eb7100fee4265a72b70036ccafd33d50e1eda5
Anwarsh2018 <anwar1991@windowslive.com>
2018-03-01 16:07:13 +0300

    events & articles forms are done

--- NFproject/Events/urls.py
+++ NFproject/Events/urls.py
@@ -4,5 +4,6 @@ from Events import views
 
 urlpatterns = [
     path('', views.events_list, name='events_list'),
+    path('add_event/', views.add_event , name='add_event'),
     path('<slug:event_slug>', views.event, name='event'),
     path('types/<slug:type_slug>', views.types, name='type')]
