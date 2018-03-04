fd4caeca8fedcbd78cfc6d45aa1b5004fbc6916f
copy4ever <copy4ever@gmail.com>
2018-03-04 09:43:35 +0300

    Change the slug library

--- NFproject/articles/urls.py
+++ NFproject/articles/urls.py
@@ -5,6 +5,6 @@ from articles import views
 urlpatterns = [
     path('', views.article_list, name='article_list'),
     path('add_article/', views.add_article , name='add_article'),
-    path('update_article/<slug:article_slug>', views.update_article, name="update_article"),
-    path('<slug:article_slug>', views.article, name='article'),
-    path('categories/<slug:category_slug>', views.category, name='category')]
+    path('update_article/<article_slug>', views.update_article, name="update_article"),
+    path('<article_slug>', views.article, name='article'),
+    path('categories/<category_slug>', views.category, name='category')]
