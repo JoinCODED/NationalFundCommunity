bb6f7235aa6020a12fa1b795b92ce863dd061db9
Anwarsh2018 <anwar1991@windowslive.com>
2018-03-05 16:24:05 +0300

    Hide somthing

--- NFproject/user_profile/forms.py
+++ NFproject/user_profile/forms.py
@@ -15,13 +15,3 @@ class OrginizationProfileForm (forms.ModelForm):
     class Meta:
         model =Orginization_Profile
         fields =['bio','my_website','location_name','location_URL']
-
-# <li class="nav-item active">
-# <a class="nav-link" href="{% url "profile_list" %}">Profiles<span class="sr-only"></span></a>
-# </li>
-# {%if profile_form.errors%}
-# <div class="form-group has-error">
-# <span class="help-block">{{profile_form.text.errors}}</span>
-# </div>
-# {% endif %}
-#{{profile_form|crispy}}
