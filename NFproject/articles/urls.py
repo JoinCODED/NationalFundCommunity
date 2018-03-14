07cf8305b6dd445301ac594d8da289323391d140
Anwarsh2018 <anwar1991@windowslive.com>
2018-03-14 15:07:49 +0300

    CBGV

--- NFproject/articles/urls.py
+++ NFproject/articles/urls.py
@@ -5,12 +5,12 @@ from articles import views
 
 app_name = 'articles'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('add/', views.add, name='add'),
-    path('<article_slug>/update/',
-         views.update, name="update"),
-    path('<int:article_id>/delete/',
-         views.delete, name="delete"),
-    path('<article_slug>', views.article, name='article'),
+    path('', views.ArticleView.as_view(), name='index'),
+    path('add/', views.AddArticle.as_view(), name='add'),
+    path('<slug>/update/',
+         views.UpdateArticle.as_view(), name="update"),
+    path('<int:pk>/delete/',
+         views.DeleteArticle.as_view(), name="delete"),
+    path('<slug>', views.ArticleDetail.as_view(), name='article'),
     path('<article_slug>/favorite/', views.favorite, name='favorite'),
     path('categories/<category_slug>', views.category, name='category')]
