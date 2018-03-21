3d9397bfec2e0db8e3c7fb9443f6474b55013106
Anwarsh2018 <anwar1991@windowslive.com>
2018-03-21 16:00:38 +0300

    Done modifying profile model

--- NFproject/user_profiles/admin.py
+++ NFproject/user_profiles/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User, Individual, Organization
+from .models import User, Individual, Organization, Industries
 
 
 admin.site.register(Individual)
 admin.site.register(Organization)
 admin.site.register(User, UserAdmin)
+admin.site.register(Industries)
